823a1851747762c0e914494396878701701926f7
Aliza Rabinovitz <alizajessa24@gmail.com>
2018-06-21 17:48:39 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,1 +1,23 @@
 # river.rb
+class River
+  def initialize(name, countries_flows_through, discharge)
+    @name = name
+
+  def countries_flows_through(countries_flows_through)
+    @countries_flows_through = countries_flows_through
+  end
+
+  def length(length)
+    @length = length
+  end
+
+  def discharge(discharge)
+    @discharge = discharge
+  end
+
+
+
+#nile egypt, sudan, south sudan, ethiopia, uganda, congo, kenya, tanzania, rwanda, burundi, eritrea
+#6853
+#2830
+
